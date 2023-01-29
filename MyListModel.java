15a89fff0ad501b63970e2f96efe5da844fb6eec
David DeCosta <DavidDeCosta12@gmail.com>
2023-01-29 15:46:42 -0500

    second commit

--- MyListModel.java
+++ MyListModel.java
@@ -2,6 +2,10 @@ import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Enumeration;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.*;
 import javax.swing.text.html.parser.ParserDelegator;
@@ -13,7 +17,13 @@ public class MyListModel extends DefaultListModel<String>
     String str;
     URLConnection urlConnection;
     InputStreamReader isr;
-    MyParserCallbackTagHandler tagHandler;
+    TagHandler tagHandler;
+
+    int testingnum = 0;
+    Vector<String> listOfUrls = new Vector<String>();
+    Vector<String> listOfEmailAdresses = new Vector<String>();
+
+    String grabTheEmailString;
 
     MyListModel()
     {
@@ -25,12 +35,13 @@ public class MyListModel extends DefaultListModel<String>
         try 
         {
             url = new URL(urlString);          // construct a url object using the string from the textfield
-//            readPage();
+            readPage();
         
             urlConnection = url.openConnection();
             isr = new InputStreamReader(urlConnection.getInputStream());
-            tagHandler = new MyParserCallbackTagHandler(urlString);
+            tagHandler = new TagHandler(urlString,listOfUrls,listOfEmailAdresses);
             new ParserDelegator().parse(isr, tagHandler, true);
+            displayElements();
             
         } 
         catch (MalformedURLException e) 
@@ -41,16 +52,37 @@ public class MyListModel extends DefaultListModel<String>
         {
             e.printStackTrace();
         }
+ 
+    }
 
-        
+    void displayElements()
+    {
+        Enumeration<String> lala = listOfUrls.elements();
+ //       System.out.println("this happened: " + testingnum + " times ");
+ //       testingnum++;
+        System.out.println("This is the list of HREF Links: \n");
+        while(lala.hasMoreElements())
+        {
+            System.out.println(lala.nextElement());
+        }
     }
 
     void readPage()
     {
+
         try 
         {
             pageReader = new BufferedReader(new InputStreamReader(url.openStream()));
             str = pageReader.readLine();            //str will hold the first line of the html file
+
+
+            Matcher matcher = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+").matcher(str);
+            if(matcher.find())
+            {
+                grabTheEmailString = matcher.group();
+                System.out.println(grabTheEmailString + "\n");
+            }
+
             while(str != null)
             {
             addElement(str);                        //add that string of text to the JList
--- TagHandler.java
+++ TagHandler.java
@@ -1,29 +1,47 @@
+import java.util.Enumeration;
+import java.util.Vector;
 import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.HTML.Tag;
 
 public class TagHandler extends HTMLEditorKit.ParserCallback
 {
     String baseDomain;
+    int testingnum = 0;
 
-    TagHandler(String baseDomain)
+    Vector<String> listOfUrls;
+    Vector<String> listOfEmailAdresses;
+
+
+    TagHandler(String baseDomain, Vector<String> listOfUrls,Vector<String> listOfEmailAdresses)
     {
         this.baseDomain = baseDomain;
+        this.listOfUrls = listOfUrls;
+        this.listOfEmailAdresses = listOfEmailAdresses;
     }
 
     TagHandler()
     {
 
     }
-
-
     
     @Override
-    public void handleSimpleTag(Tag t, MutableAttributeSet a, int pos) 
+    public void handleStartTag(Tag t, MutableAttributeSet a, int pos) 
     {
-        
-        handleSimpleTag(t, a, pos);
 
-        
+ //       System.out.println("Found a tag: " + t);
+
+        Enumeration<?> attEnum;
+        attEnum = a.getAttributeNames();
+        while(attEnum.hasMoreElements())
+        {
+            if(attEnum.nextElement().equals(HTML.Attribute.HREF))
+            {
+            //    System.out.println(a.getAttribute(HTML.Attribute.HREF) + "\n");
+                listOfUrls.addElement((String) a.getAttribute(HTML.Attribute.HREF));
+            }
+        }
     }
+     
 }
